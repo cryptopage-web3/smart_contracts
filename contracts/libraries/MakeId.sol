9b084f932118ea4607a0f866f7f26d7dcdf2c712
vpomo <pomogalov@gmail.com>
2023-01-13 10:30:06 +0300

    added library for SoulBound id

---
+++ contracts/libraries/MakeId.sol
@@ -0,0 +1,23 @@
+// SPDX-License-Identifier: MIT
+
+pragma solidity 0.8.15;
+
+
+library MakeId {
+
+    uint256 constant ADDITIONAL_ID = 1_000_000;
+
+    function getRateId(uint256 _tokenId) internal view returns(uint256) {
+        return _tokenId % (ADDITIONAL_ID * getChainId());
+    }
+
+    function getSoulBoundTokenId(address _value, uint256 _rateId) internal view returns(uint256) {
+        return uint256(uint160(_value)) * ADDITIONAL_ID * getChainId() + _rateId;
+    }
+
+    function getChainId() private view returns (uint256 chainId) {
+        assembly {
+            chainId := chainid()
+        }
+    }
+}
